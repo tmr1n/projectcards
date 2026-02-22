5c3cd88ff6f035b110498f556418ea3bffdab149
tmr1n <163327345+tmr1n@users.noreply.github.com>
2026-02-22 20:19:46 +0100

    Add forgot password page

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,13 +1,33 @@
 'use client'
 
-import ForgotPasswordForm from '@/components/ForgotPasswordForm'
+import { ArrowLeft, X } from 'lucide-react'
+import { FirstSideComponent } from '@/components/FirstSideComponent'
+import { ForgotPasswordForm } from '@/components/ForgotPasswordForm'
 
 interface Props {}
 
 export default function page({}: Props) {
 	return (
-		<div>
-			<ForgotPasswordForm />
+		<div className='flex flex-row h-screen'>
+			<FirstSideComponent text='У вас всё получится!' />
+			<div className='w-[50%] overflow-y-auto flex flex-col'>
+				<div className='flex justify-between p-4'>
+					<ArrowLeft
+						color='#586380'
+						size={32}
+						strokeWidth={1.5}
+						className='hover:scale-110 hover:opacity-80 duration-300 cursor-pointer'
+					/>
+					<X
+						color='#586380'
+						size={32}
+						strokeWidth={1.5}
+						className='hover:scale-110 hover:opacity-80 duration-300 cursor-pointer'
+					/>
+				</div>
+
+				<ForgotPasswordForm example={''} exampleRequired={''} />
+			</div>
 		</div>
 	)
 }
--- src/components/ForgotPasswordForm.tsx
+++ src/components/ForgotPasswordForm.tsx
@@ -1,11 +1,51 @@
-import { FirstSideComponent } from '@/components/FirstSideComponent'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+import { ButtonSubmit } from '@/components/buttons/ButtonSubmit'
+import { Input } from '@/components/form/Input'
+import { LabelComponent } from './form/LabelComponent'
 
-interface Props {}
+interface IForgotPasswordFormProps {
+	example: string
+	exampleRequired: string
+}
 
-export default function ForgotPasswordForm({}: Props) {
+export function ForgotPasswordForm({}: IForgotPasswordFormProps) {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors }
+	} = useForm<IForgotPasswordFormProps>()
+	const onSubmit: SubmitHandler<IForgotPasswordFormProps> = data =>
+		console.log(data)
+
 	return (
-		<div className='flex flex-row h-screen'>
-			<FirstSideComponent text='У вас всё получится!' />
+		<div className=' bg-white flex items-center justify-center p-8 '>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='w-full max-w-lg flex flex-col gap-4'
+			>
+				<div>
+					<h1 className='text-3xl font-bold text-black font-nunito mb-4'>
+						Выполнить сброс пароля
+					</h1>
+					<p className='font-nunito text-black'>
+						Введите адрес электронной почты, использованный при регистрации. Мы
+						отправим вам ссылку для входа и сброса пароля. Если вы
+						зарегистрировались с помощью адреса электронной почты родителей, мы
+						отправим им эту ссылку.
+					</p>
+				</div>
+				<div className='space-y-2'>
+					<LabelComponent text={'Email'}></LabelComponent>
+					<Input placeholder='user@mail.com'></Input>
+				</div>
+
+				<ButtonSubmit
+					variant='primary'
+					text={'Отправить ссылку для сброса'}
+					type='submit'
+				></ButtonSubmit>
+			</form>
 		</div>
 	)
 }
