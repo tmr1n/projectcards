a349dafbbed446f0d30dcda58cc0763623eb929f
tmr1n <163327345+tmr1n@users.noreply.github.com>
2026-02-25 21:25:31 +0100

    zalupa

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,10 +3,10 @@ import { ButtonMain } from '@/components/buttons/ButtonMain'
 export default function Home() {
 	return (
 		<div className='flex flex-col items-center justify-center gap-4 p-4'>
-			<h1 className='text-[2.75rem] font-bold text-black font-nunito '>
+			<h1 className='text-[2.5rem] text-center md:text-[2.75rem] font-bold text-black font-nunito '>
 				Как вы хотите заниматься?
 			</h1>
-			<p className='text-[1.25rem] text-center text-black font-nunito'>
+			<p className='text-[1rem] md:text-[1.25rem] text-center text-black font-nunito'>
 				Освойте любой изучаемый материал с помощью интерактивных карточек,
 				<br />
 				пробных тестов и учебных активностей.
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,16 +4,23 @@ import { ButtonMain } from '../buttons/ButtonMain'
 
 export function Header() {
 	return (
-		<header className='flex items-center justify-between px-2 py-2'>
-			<Link href='/' className='flex items-center gap-2'>
-				<Image
-					src='/images/Logo.svg'
-					alt='Project Cards Logo'
-					width={113}
-					height={76}
-				/>
-			</Link>
-			<ButtonMain text={'Вход'} href='/login'></ButtonMain>
+		<header className='flex px-2 py-2 items-center justify-between'>
+			<div className='flex justify-start'>
+				<Link href='/' className='gap-2 '>
+					<Image
+						src='/images/Logo.svg'
+						alt='Project Cards Logo'
+						width={113}
+						height={76}
+					/>
+				</Link>
+			</div>
+
+			<div className='justify-end'>
+				<ButtonMain text={'Вход'} href='/login'></ButtonMain>
+			</div>
 		</header>
 	)
 }
+
+//залупа, переделатть
