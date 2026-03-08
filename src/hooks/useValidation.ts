6bc78b88ba992d77859c01f2a695a5a1dda07b28
tmr1n <163327345+tmr1n@users.noreply.github.com>
2026-03-08 18:44:27 +0100

    Validation start

---
+++ src/hooks/useValidation.ts
@@ -0,0 +1,16 @@
+import { useForm, UseFormReturn } from 'react-hook-form'
+
+export interface LoginFormData {
+	email: string
+	password: string
+}
+
+export const useLoginValidation = (): UseFormReturn<LoginFormData> => {
+	return useForm<LoginFormData>({
+		mode: 'onChange',
+		defaultValues: {
+			email: '',
+			password: ''
+		}
+	})
+}
