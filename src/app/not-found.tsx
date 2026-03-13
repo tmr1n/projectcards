847245e4df223cb8410495f23b3349d57bd83cb6
tmr1n <163327345+tmr1n@users.noreply.github.com>
2026-03-13 13:48:22 +0100

    update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
 	return (
-		<div className='grid min-h-full place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8'>
+		<div className='grid place-items-center min-h-screen bg-gray-900 px-6 sm:py-32 lg:px-8'>
 			<div className='text-center'>
 				<p className='text-base font-semibold text-indigo-400'>404</p>
 				<h1 className='mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl'>
@@ -11,7 +11,7 @@ export default function NotFound() {
 				</p>
 				<div className='mt-10 flex items-center justify-center gap-x-6'>
 					<a
-						href='#'
+						href='/'
 						className='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
 					>
 						Go back home
