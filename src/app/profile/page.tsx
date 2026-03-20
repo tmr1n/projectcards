44efca9231431c79b8598f266e8e55b2694d08f8
tmr1n <163327345+tmr1n@users.noreply.github.com>
2026-03-20 21:25:03 +0100

    feat: start profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,9 @@
 interface Props {}
 
 export default function page({}: Props) {
-	return <div>page</div>
+	return (
+		<div className=''>
+			<div className='bg-gray-300 mr-10 p-30 w-2 h-screen'></div>
+		</div>
+	)
 }
